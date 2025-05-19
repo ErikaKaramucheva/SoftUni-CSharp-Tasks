fa628220f5255d1935c0e17f69ea60cec1e7aa1f
ErikaKaramucheva <105073095+ErikaKaramucheva@users.noreply.github.com>
2025-05-19 18:52:28 +0300

    Add solutions from "PB - Exam" module

---
+++ Basics/PB - Exam 2/Program.cs
@@ -0,0 +1,207 @@
+﻿//1 - Oscars Ceremony
+/*int rent= int.Parse(Console.ReadLine());
+double statuettes = rent - (0.3 * rent);
+double catering = statuettes-(0.15*statuettes);
+double sound = catering / 2;
+double cost = rent + statuettes + catering + sound;
+Console.WriteLine(Math.Round(cost,2));*/
+
+//2 - Godzilla vs Kong
+/*double budget = double.Parse(Console.ReadLine());
+int statists = int.Parse(Console.ReadLine());
+double priceClothesForOneStatist = double.Parse(Console.ReadLine());
+double priceForAllClothes = priceClothesForOneStatist * statists;
+double decor = 0.1 * budget;
+if (statists > 150)
+{
+    priceForAllClothes-=0.1*priceForAllClothes;
+}
+double totalPrice= decor+priceForAllClothes;
+if(totalPrice > budget)
+{
+    Console.WriteLine("Not enough money!");
+    Console.WriteLine($"Wingard needs {Math.Round((totalPrice-budget),2)} leva more.");
+}
+else
+{
+    Console.WriteLine("Action!");
+    Console.WriteLine($"Wingard starts filming with {Math.Round((budget-totalPrice),2)} leva left.");
+}*/
+
+//3 - Oscars week in cinema
+/*string name = Console.ReadLine();
+string hallType = Console.ReadLine();
+int tickets = int.Parse(Console.ReadLine());
+double price = 0;
+switch (name) {
+    case "A Star Is Born":
+        if (hallType.Equals("normal"))
+        {
+            price = 7.5;
+        }else if (hallType.Equals("luxury"))
+        {
+            price = 10.5;
+        }
+        else
+        {
+            price = 13.5;
+        }
+        break;
+    case "Bohemian Rhapsody":
+        if (hallType.Equals("normal"))
+        {
+            price = 7.35;
+        }
+        else if (hallType.Equals("luxury"))
+        {
+            price = 9.45;
+        }
+        else
+        {
+            price = 12.75;
+        }
+        break;
+    case "Green Book":
+        if (hallType.Equals("normal"))
+        {
+            price = 8.15;
+        }
+        else if (hallType.Equals("luxury"))
+        {
+            price = 10.25;
+        }
+        else
+        {
+            price = 13.25;
+        }
+        break;
+    default:
+        if (hallType.Equals("normal"))
+        {
+            price = 8.75;
+        }
+        else if (hallType.Equals("luxury"))
+        {
+            price = 11.55;
+        }
+        else
+        {
+            price = 13.95;
+        }
+        break;
+}
+double total = tickets * price;
+Console.WriteLine($"{name} -> {Math.Round(total,2)} lv.");*/
+
+//4 - Cinema voucher
+/*int voucher = int.Parse(Console.ReadLine());
+int tickets = 0;
+int otherItems = 0;
+string command = Console.ReadLine(); ;
+while (!command.Equals("End"))
+{
+    int price = 0;
+    bool isTicket = true;
+    if (command.Length > 8)
+    {
+        tickets++;
+       price= command[0] + command[1];
+    }
+    else
+    {
+        otherItems++;
+        price= command[0];
+        isTicket = false;
+    }
+    if (price <= voucher)
+    {
+        voucher -= price;
+    }
+    else
+    {
+        if (isTicket)
+        {
+            tickets--;
+        }
+        else
+        {
+            otherItems--;
+        }
+        break;
+    }
+    command = Console.ReadLine();
+}
+Console.WriteLine(tickets);
+Console.WriteLine(otherItems);*/
+
+//5 - Movie Ratings
+/*int filmsCount = int.Parse(Console.ReadLine());
+double highestRating = double.MinValue;
+double lowestRating = double.MaxValue;
+string lowestRatingName = "";
+string highestRatingName = "";
+double average = 0;
+for(int i=0;i<filmsCount; i++)
+{
+    string name=Console.ReadLine();
+    double rating=double.Parse(Console.ReadLine());
+    average += rating;
+    if (highestRating < rating)
+    {
+        highestRating = rating;
+        highestRatingName=name;
+    }else if (lowestRating > rating)
+    {
+       lowestRating = rating;
+        lowestRatingName = name;
+    }
+
+}
+Console.WriteLine($"{highestRatingName} is with highest rating: {Math.Round(highestRating,1)}");
+Console.WriteLine($"{lowestRatingName} is with lowest rating: {Math.Round(lowestRating,1)}");
+Console.WriteLine($"Average rating: {Math.Round(average/filmsCount,1)}");*/
+
+//6 - Cinema Tickets
+string name = Console.ReadLine();
+int freeSlots;
+int totalTickets = 0;
+int studentsTickets = 0;
+int standardTickets = 0;
+int kidsTickets = 0;
+while (!name.Equals("Finish"))
+{
+    freeSlots = int.Parse(Console.ReadLine());
+    string command=Console.ReadLine();
+    int fullSlots = 0;
+    while (!command.Equals("End"))
+    {
+        fullSlots++;
+        totalTickets++;
+        switch (command)
+        {
+            case "student":
+                studentsTickets++;
+                break;
+            case "standard":
+                standardTickets++;
+                break;
+            default:
+                kidsTickets++;
+                break;
+        }
+        if (fullSlots == freeSlots)
+        {
+            break;
+        }
+        command = Console.ReadLine();
+    }
+    double fullnessPercent = (double)fullSlots / freeSlots * 100;
+    Console.WriteLine($"{name} - {Math.Round(fullnessPercent,2)}% full.");
+
+    name = Console.ReadLine();
+}
+Console.WriteLine($"Total tickets: {totalTickets}");
+Console.WriteLine(Math.Round((double)studentsTickets / totalTickets * 100, 2) + "% student tickets.");
+Console.WriteLine(Math.Round((double)standardTickets / totalTickets * 100, 2) + "% standard tickets.");
+Console.WriteLine(Math.Round((double)kidsTickets / totalTickets * 100, 2) + "% kids tickets.");
+
---
+++ Basics/PB - Exam 8/Program.cs
@@ -0,0 +1,148 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        //01
+        /*double strawberryPrice=double.Parse(Console.ReadLine());
+        double kgBananas=double.Parse(Console.ReadLine());
+        double kgOranges=double.Parse(Console.ReadLine());
+        double kgRedberries = double.Parse(Console.ReadLine());
+        double kgStrawberries=double.Parse(Console.ReadLine());
+
+        double redberriesPrice = strawberryPrice / 2;
+        double orangesPrice=redberriesPrice-(0.4*redberriesPrice);
+        double bananasPrice = redberriesPrice - (0.8 * redberriesPrice);
+
+        double strawberrySum = strawberryPrice * kgStrawberries;
+        double bananasSum = bananasPrice * kgBananas;
+        double orangesSum= orangesPrice * kgOranges;
+        double redberriesSum = redberriesPrice * kgRedberries;
+        double totalSum=strawberrySum+ bananasSum+orangesSum+redberriesSum;
+        Console.WriteLine(Math.Round(totalSum,2));*/
+
+        //02
+        /*double budget = double.Parse(Console.ReadLine());
+        double neededFuel = double.Parse(Console.ReadLine());
+        string day= Console.ReadLine();
+        double sumForFuel = neededFuel * 2.1;
+        double totalSum = sumForFuel + 100;
+        if (day.Equals("Saturday"))
+        {
+            totalSum -= 0.1 * totalSum;
+        }
+        else
+        {
+            totalSum -= 0.2 * totalSum;
+        }
+        if (budget >= totalSum)
+        {
+            Console.WriteLine($"Safari time! Money left {Math.Round((budget - totalSum), 2)} lv.");
+        }
+        else
+        {
+            Console.WriteLine($"Not enough money! Money needed: {Math.Round(totalSum - budget, 2)} lv.");
+        }*/
+
+        //03
+        /*string duration = Console.ReadLine();
+        string type = Console.ReadLine();
+        string internet = Console.ReadLine();
+        int months = int.Parse(Console.ReadLine());
+        double sum = 0;
+        if (duration.Equals("one")) {
+            switch (type)
+            {
+                case "Small": sum = 9.98; break;
+                case "Middle": sum = 18.99; break;
+                case "Large": sum = 25.98; break;
+                default: sum = 35.99; break;
+            }
+        }
+        else
+        {
+            switch (type)
+            {
+                case "Small": sum = 8.58; break;
+                case "Middle": sum = 17.09; break;
+                case "Large": sum = 23.59; break;
+                default: sum = 31.79; break;
+            }
+        }
+
+        if (internet.Equals("yes"))
+        {
+            if (sum <= 10)
+            {
+                sum += 5.5;
+            }else if(sum>10 && sum <= 30)
+            {
+                sum += 4.35;
+            }else if (sum > 30)
+            {
+                sum += 3.85;
+            }
+        }
+
+        sum = sum * months;
+        if (duration.Equals("two"))
+        {
+            sum -= sum * 0.0375;
+        }
+        Console.WriteLine(Math.Round(sum, 2) + " lv.");*/
+
+        //04
+        /*double budget= double.Parse(Console.ReadLine());
+        string command=Console.ReadLine();
+        double price;
+        double sum = 0;
+        int items = 0;
+        int count = 0;
+        while (!command.Equals("Stop"))
+        {
+            price= double.Parse(Console.ReadLine());
+            count++;
+            if (count == 3)
+            {
+                price = price / 2;
+            }
+            if (price+sum > budget)
+            {
+                Console.WriteLine("You don't have enough money!");
+                Console.WriteLine($"You need {(price+sum)-budget} leva!");
+                return;
+            }
+            sum+= price;
+            items++;
+            command = Console.ReadLine();
+        }
+        Console.WriteLine($"You bought {items} products for {sum} leva.");*/
+
+        //06
+        int days = int.Parse(Console.ReadLine());
+        int hours = int.Parse(Console.ReadLine());
+        double currentSum = 0;
+        double sum = 0;
+        for (int i = 1; i <= days; i++)
+        {
+            currentSum = 0;
+            for (int j = 1; j <= hours; j++)
+            {
+                if(i%2==0 && j % 2 == 1)
+                {
+                    currentSum += 2.5;
+                }else if( i%2==1 & j % 2 == 0)
+                {
+                    currentSum += 1.25;
+                }
+                else
+                {
+                    currentSum += 1;
+                }
+            }
+            Console.WriteLine($"Day: {i} - {currentSum} leva");
+            sum += currentSum;
+        }
+        Console.WriteLine($"Total - {sum} leva");
+
+    }
+}
